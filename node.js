56a0016b18d47ea9c5700b94f70240a1f052c3e8
raff1nk <135601305+raff1nk@users.noreply.github.com>
2023-06-17 22:18:19 +0700

    Add files via upload

---
+++ node.js
@@ -0,0 +1,14 @@
+//togle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// hamburger menu click
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+// klick di luar sidebar
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
